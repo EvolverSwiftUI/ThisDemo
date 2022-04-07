cf14a7350eeba27d14831b9b951ed3d05391c763
Sivaram Yadav <sivaramaiah.nakka@concentrix.com>
2022-04-07 18:52:47 +0530

    addons on js concepts

--- index.js
+++ index.js
@@ -118,3 +118,75 @@ Objects:
     3. Function    
 */
 
+let x = 5;
+let y = x;
+y = 10;
+
+console.log(x);
+console.log(y);
+
+let a = {value: 5};
+let b = a;
+b.value = 10;
+console.log(a);
+let c = {value: 20};
+b = c;
+console.log(b);
+console.log(c);
+
+// Declaring variables in JS
+/*
+It can be done in 3 ways
+    1. using let
+    2. using const
+    3. using var
+*/
+
+// 1. using let
+// 1.1. initialization is not mandetory
+// 1.2. variables can be reassigned
+
+let k;
+k = 10;
+console.log(k);
+k = 15;
+console.log(k);
+
+// 2. using const
+// 2.1. initialization is mandetory
+// 2.2. can't be reassigned
+
+const i = 400;
+// i = 20; ===> gives syntax error
+console.log(i);
+
+// mutating object properties
+const bike = {
+    color: "green",
+    brand: "Honda"
+};
+
+bike.color = "black";
+console.log(bike);
+
+bike = {};
+// console.log(bike); ===> Type error
+
+// Summary:
+/*
+1. This
+    1.1. Object methods
+    1.2. Regular functions
+    1.3. Arrow functions
+    1.4. Constructor functions
+    1.5. Callbacks
+2. JS Values
+    2.1. Immutable
+    2.2. Mutable
+    2.3. new operator
+3. Declaring variables
+    3.1. const
+    3.2. let vs const        
+*/
+
+
